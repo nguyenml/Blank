f1c45ec4cb9cb20d99b750711136f84b9cdfdaf9
Marvin Nguyen <marvin@marvins-macbook.local>
2017-03-08 19:16:46 -0500

    Initial Commit

---
+++ Blanket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blanket
+//
+//  Created by Marvin Nguyen on 3/8/17.
+//  Copyright © 2017 Marvin Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
