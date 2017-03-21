e51f7252bea1d55221da752ca7e4ab3684ae570e
Marvin Nguyen <marvin@Marvins-MacBook.local>
2017-03-21 18:10:48 -0400

    new

---
+++ Blanket/CollectionCellView.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionCellView.swift
+//  
+//
+//  Created by Marvin Nguyen on 3/21/17.
+//
+//
+
+import UIKit
+
+class CollectionCellView: UICollectionViewCell {
+    
+    @IBOutlet weak var BadgeLabel: UILabel!
+}
